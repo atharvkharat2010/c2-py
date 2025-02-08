05c55ee03683e25b2d22a0fd35584474ae33e437
atharvkharat2010 <atharvabk1999@gmail.com>
2025-02-08 21:12:51 +0530

    Add files via upload

---
+++ act4.py
@@ -0,0 +1,6 @@
+import datetime
+current_time=datetime.datetime.now()
+print("time now at greenwich meridian is: ",end="")
+print(current_time)
+import calendar
+print("\n" ,calendar.calendar(2025))
